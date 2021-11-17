e0e3d0a167794e4d59a36f00ac9474e760f5ff07
Richmond Kweku Nketiah <92983755+Sterling619@users.noreply.github.com>
2021-11-18 02:18:10 +0800

    Update style.js

--- src/comp/style.js
+++ src/comp/style.js
@@ -1,1 +1,20 @@
+import { StyleSheet } from 'react-native'
 
+export default StyleSheet.create({
+  cartList: {
+    marginBottom: 75,
+  },
+  textRight: {
+    textAlign: 'right',
+    fontSize: 21,
+    padding: 10,
+    color: '#ff2e63',
+    fontStyle: 'italic',
+  },
+  textMessage: {
+    fontSize: 16,
+    color: 'green',
+    textAlign: 'center',
+    justifyContent: 'center',
+  }
+})
